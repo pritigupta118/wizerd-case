c419875fca776b23758c92a89340ba43bd63fced
Priti Gupta <civilstudent9900@gmail.com>
2024-08-24 20:24:09 +0530

    creating our stripe webhooks

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,4 +1,9 @@
+import { db } from "@/db"
+import { stripe } from "@/lib/stripe"
+import { ok } from "assert"
 import { headers } from "next/headers"
+import { NextResponse } from "next/server"
+import Stripe from "stripe"
 
 export async function POST(req: Request) {
   try {
@@ -8,7 +13,69 @@ export async function POST(req: Request) {
     if (!signature) {
       return new Response
     }
+    const event = stripe.webhooks.constructEvent(
+      body, 
+      signature, 
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+
+    if (event.type === 'checkout.session.completed') {
+      if (!event.data.object.customer_details?.email) {
+        throw new Error("Missing user email")
+      }
+
+      const session = event.data.object as Stripe.Checkout.Session
+
+      const {userId, orderId} = session.metadata  || {
+        userId: null,
+        orderId: null
+      }
+
+      if (!userId || !orderId) {
+        throw new Error("Invalid request metadata")
+      }
+
+      const billingAddress = session.customer_details!.address
+      const shippingAddress = session.shipping_details!.address
+
+      const updatedOrder = await db.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          isPaid: true,
+          shippingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: shippingAddress!.city!,
+              country: shippingAddress!.country!,
+              postalCode: shippingAddress!.postal_code!,
+              street: shippingAddress!.line1!,
+              state: shippingAddress!.state,
+            },
+          },
+          billingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: billingAddress!.city!,
+              country: billingAddress!.country!,
+              postalCode: billingAddress!.postal_code!,
+              street: billingAddress!.line1!,
+              state: billingAddress!.state,
+            },
+          },
+        },
+      })
+    }
+
   } catch (error) {
-    
+    console.error(error)
+    return NextResponse.json({
+      error: "Failed to update order",
+      ok: false
+    }, 
+      {status: 500}
+    )
+
   }
 }
