75a0a00165c451549364b2255f79f018bff91e14
Priti Gupta <civilstudent9900@gmail.com>
2024-08-26 19:48:21 +0530

    writing our thank-you email

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,7 +3,11 @@ import { stripe } from "@/lib/stripe"
 import { headers } from "next/headers"
 import { NextResponse } from "next/server"
 import Stripe from "stripe"
+import {Resend} from "resend"
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail"
 
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 export async function POST(req: Request) {
   try {
     const body = await req.text()
@@ -37,7 +41,7 @@ export async function POST(req: Request) {
       const billingAddress = session.customer_details!.address
       const shippingAddress = session.shipping_details!.address
 
-      await db.order.update({
+      const updateOrder = await db.order.update({
         where: {
           id: orderId,
         },
@@ -64,6 +68,25 @@ export async function POST(req: Request) {
             },
           },
         },
+      })
+
+      await resend.emails.send({
+        from: 'WizerdCase <civilstudent9900@gmail.com>',
+        to: [event.data.object.customer_details.email],
+        subject: 'Thanks for your order!',
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updateOrder.createdAt.toLocaleDateString(),
+          //@ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          }
+        })
       })
     }
     return NextResponse.json({result: event, ok: true})
